cd65fc8639252175688b198b99c2eb9a56ba609c
Uichae <ghddmlco@gmail.com>
2023-06-23 16:46:21 +0900

    투두리스트, App.js 수정

--- 230623/src/App.js
+++ 230623/src/App.js
@@ -1,10 +1,12 @@
 import LoginForm from "component/login";
+import Todos from "component/Practice_todolist";
 
 function App() {
   return (
     <div>
       hello world
       <LoginForm />
+      <Todos />
     </div>
   );
 }
