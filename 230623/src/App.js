f30109c6175b542e5e4e2ce190718c0aaaa1ca59
sweeeeetpotato <sweet_potato13@naver.com>
2023-06-26 10:20:13 +0900

    이미지 슬라이더 구현

--- 230623/src/App.js
+++ 230623/src/App.js
@@ -1,20 +1,15 @@
-<<<<<<< HEAD
-import LoginForm from "./component/login";
-import RandomNumber from "./component/randomnumber";
-=======
 import LoginForm from "component/login";
+import RandomNumber from "component/randomnumber";
 import Todos from "component/Practice_todolist";
->>>>>>> cd65fc8639252175688b198b99c2eb9a56ba609c
+import ImageSlider from "component/imgSlider";
 
 function App() {
   return (
     <div>
       <LoginForm />
-<<<<<<< HEAD
       <RandomNumber />
-=======
       <Todos />
->>>>>>> cd65fc8639252175688b198b99c2eb9a56ba609c
+      <ImageSlider />
     </div>
   );
 }
