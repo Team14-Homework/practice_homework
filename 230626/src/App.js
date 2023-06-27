3af719fb6ae728b7b848ffd798c15948894ebe2f
sweeeeetpotato <sweet_potato13@naver.com>
2023-06-28 00:38:39 +0900

    Feat : 영화 검색 기능 & 영화 상세페이지 기능 구현

--- 230626/src/App.js
+++ 230626/src/App.js
@@ -1,5 +1,17 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SearchResults from "pages/SearchResults";
+import MovieDetail from "pages/MovieDetail";
+
 function App() {
-  return <div>테스트</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/search" element={<SearchResults />}></Route>
+        <Route path="/detail/:movieId" element={<MovieDetail />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 export default App;
---
+++ 230626/src/pages/SearchResults.jsx
@@ -0,0 +1,14 @@
+import HomeFooter from "components/HomeFooter";
+import HomeHeader from "components/HomeHeader";
+import SearchMain from "components/SearchMain";
+import React from "react";
+
+export default function SearchResults() {
+  return (
+    <>
+      <HomeHeader />
+      <SearchMain />
+      <HomeFooter />
+    </>
+  );
+}
