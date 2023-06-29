35acb158e6566f6565beb4edfa15091a03d33417
sweeeeetpotato <sweet_potato13@naver.com>
2023-06-29 19:05:49 +0900

    Style : 가로스크롤이 생기는 문제 해결

--- 230626/src/pages/MovieDetail.jsx
+++ 230626/src/pages/MovieDetail.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import HomeFooter from "components/HomeFooter";
 import HomeHeader from "components/HomeHeader";
 import DetailMain from "components/DetailMain";
+import "styles/reset.css";
 import styles from "styles/layout.module.css";
 
 export default function MovieDetail() {
