037298509f41910c76287a843a1e9d64cc395c3e
sweeeeetpotato <sweet_potato13@naver.com>
2023-06-30 23:02:31 +0900

    Style : 공통 스타일을 common폴더로 분리

--- 230626/src/pages/Home.jsx
+++ 230626/src/pages/Home.jsx
@@ -2,12 +2,10 @@ import React from "react";
 import HomeFooter from "components/HomeFooter";
 import HomeHeader from "components/HomeHeader";
 import HomeMain from "components/HomeMain";
-import "styles/reset.css";
-import styles from "styles/layout.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.layout}>
+    <div className="layout">
       <HomeHeader />
       <HomeMain />
       <HomeFooter />
